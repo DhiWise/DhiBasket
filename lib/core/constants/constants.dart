f51b97b173c8da4912a307475db06cd549d47b24
Saloni Saraiya <saloni.saraiya@dhiwise.com>
2022-09-05 09:50:28 +0530

    Create constants.dart

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,8 @@
+class ApiData {
+  String token = '---replace with your token---';
+  String CategoryCollectionId = '---replace with your id---';
+  String ProductCollectionId = '---replace with your id---';
+  String WishListCollectionId = '---replace with your id---';
+  String UserCollectionId = '---replace with your id---';
+  String AddressCollectionId = '---replace with your id---';
+}
